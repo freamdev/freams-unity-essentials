c282728833105ee6c2eaf31e60969f4db2952246
Richard Pinter <freamdev@gmail.com>
2018-01-18 20:40:32 +0100

    Added billboard and ScriptableObject utility classes

---
+++ Billboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Rotates the object towards the camera.
+/// How to use: Add the MonoBehaviour to the UI element that needs to face the camera.
+/// </summary>
+public class Billboard : MonoBehaviour
+{
+    [Tooltip("The UI element will face this Camera. Defaults to the Main Camera if left empty")]
+    public Camera MyCamera;
+
+    void Start()
+    {
+        if(MyCamera == null)
+        {
+            Debug.Log("No Camera was selected for " + gameObject.name + " setting Main Camera");
+            MyCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        var transform = GetComponent<Transform>();
+        transform.LookAt(transform.position + MyCamera.transform.rotation * Vector3.forward,
+                        MyCamera.transform.rotation * Vector3.up);
+    }
+}
---
+++ ScriptableObjectUtility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+/// <summary>
+/// Creates a ScriptableObject based on the T parameter
+/// How to use: Simple place it into your Unity project and call CreateAsset when required.
+/// </summary>
+public static class ScriptableObjectUtility
+{
+    public static ScriptableObject CreateAsset<T>() where T : ScriptableObject
+    {
+        T asset = ScriptableObject.CreateInstance<T>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(T).ToString() + ".asset");
+
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+
+        return asset;
+    }
+
+    
+}
